27084370e5ac42a402791443d9ce957928c33764
Pegah <125195943+Pegah111@users.noreply.github.com>
2023-05-03 16:06:02 +0100

    directory.rb saved the data

--- directory.rb
+++ directory.rb
@@ -1,15 +1,13 @@
 @students = []
-
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    
     name = gets.chomp
-   
+    
     while !name.empty? do
         students << {name: name, cohort: :november}
         puts "Now we have #{students.count} students"
-        
+
         name = gets.chomp
     end
 end
@@ -24,6 +22,7 @@ end
         def print_menu
             puts "1. Input the students"
             puts "2. Show the students"
+            puts "3. Save the list to students.csv"
             puts "9. Exit" 
           end
 
@@ -39,6 +38,8 @@ end
         input_students
     when "2"
         show_students
+    when "3"
+        save_students
     when "9"
       exit 
     else
@@ -51,11 +52,17 @@ def print_header
     puts "-------------"
 end
 
-def print_students_list
-    students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def save_students
+    
+    file = File.open("students.csv", "w")
+   
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
     end
-end
+    file.close
+  end
     
 def print_footer
     puts "Overall, we have #{students.count} great students"
